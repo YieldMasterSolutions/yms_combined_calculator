2077da3ba1d2c04dbda187b263816f58e0fb5c45
John Bailey <jbailey@legendseeds.net>
2025-05-16 13:56:43 -0500

    ✅ Added trailingSlash to next.config.js for GitHub routing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-﻿/** @type {import('next').NextConfig} */
+﻿// next.config.js
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: '/yms_combined_calculator',
   assetPrefix: '/yms_combined_calculator/',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
