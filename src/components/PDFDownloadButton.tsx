b949af4c4d66e99ff71c990feb8a7f50ed23d7fd
John Bailey <jbailey@legendseeds.net>
2025-05-23 12:01:45 -0500

    ✅ Fix type error in PDFDownloadButton and finalize deployment

--- src/components/PDFDownloadButton.tsx
+++ src/components/PDFDownloadButton.tsx
@@ -7,7 +7,7 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 interface PDFDownloadButtonProps {
-  targetRef: React.RefObject<HTMLDivElement>;
+  targetRef: React.RefObject<HTMLDivElement | null>;
 }
 
 const PDFDownloadButton: React.FC<PDFDownloadButtonProps> = ({ targetRef }) => {
